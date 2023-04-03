3dd67deff8f3aaba307108c868009b92bbaaf724
Eli Front <elifront23@gmail.com>
2023-04-03 03:20:29 -0500

    update

--- lua/elifront/remap.lua
+++ lua/elifront/remap.lua
@@ -1,16 +1,17 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- leader s to write
-vim.keymap.set("n", "<leader>sv", "<cmd>w<CR>")
-
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
--- make leader s be mapped to nothing
-vim.keymap.set("n", "<leader>s", "")
+vim.keymap.set("n", "<leader>sv", "<cmd>w<CR>")
+vim.keymap.set("n", "<C-s>", function()
+    vim.cmd("w")
+end)
 
 
+-- control shift f
+vim.keymap.set("n", "<CS-f>", "<cmd>lua require('telescope.builtin').grep_string()<CR>")
