1edf25c64d300771312fe8bf002cc95182d66a5d
Eli Front <elifront23@gmail.com>
2024-01-24 22:53:29 -0600

    copilot

--- lua/elifront/plugins/copilot.lua
+++ lua/elifront/plugins/copilot.lua
@@ -19,7 +19,19 @@ return {
         "zbirenbaum/copilot-cmp",
         dependencies = "copilot.lua",
         opts = {},
-        config = true,
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client.name == "copilot" then
+                copilot_cmp._on_insert_enter({})
+              end
+            end,
+          })
+
+          copilot_cmp.setup(opts)
+        end,
       },
     },
   },
