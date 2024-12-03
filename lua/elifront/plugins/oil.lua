ed4434db28c2cff4ab60351e5f3ee41ec52214a5
Eli Front <elifront23@gmail.com>
2024-12-02 20:48:21 -0600

    hide gitignored files

--- lua/elifront/plugins/oil.lua
+++ lua/elifront/plugins/oil.lua
@@ -6,15 +6,20 @@ return {
       default_file_explorer = true,
       columns = {
         "icon",
+        "git_status"
       },
       view_options = {
-        show_hidden = true
+        show_hidden = true,
+        is_hidden_file = function(file)
+          -- Check if the file is gitignored
+          local gitignore_cmd = string.format("git check-ignore %q", file)
+          local output = vim.fn.systemlist(gitignore_cmd)
+
+          -- If output is non-empty, the file is ignored
+          return #output > 0
+        end,
       },
     })
-
-
-    -- OilGitIgnored
-
 
     vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { silent = true })
   end
