d905bbc8b8e703430ca37009247fcb2be4396732
Eli Front <elifront23@gmail.com>
2025-10-18 18:00:12 -0500

    feat: add python treesitter

--- lua/elifront/plugins/treesitter.lua
+++ lua/elifront/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "vimdoc", "markdown", "markdown_inline", "rust", "c", "lua", "vim", "typescript", "javascript", "html", "jsdoc", "hcl", "terraform", "json5", "svelte", "php", "css", "tsx", "go", "zig" },
+      ensure_installed = { "vimdoc", "markdown", "markdown_inline", "rust", "c", "lua", "vim", "typescript", "javascript", "html", "jsdoc", "hcl", "terraform", "json5", "svelte", "php", "css", "tsx", "go", "zig", "python" },
       sync_install = false,
       auto_install = false,
       highlight = { enable = true },
