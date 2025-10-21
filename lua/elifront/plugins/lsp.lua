8ff0fdeae46bb119740724a676effdff431a6ac2
Eli Front <elifront23@gmail.com>
2025-10-21 16:49:11 -0500

    feat: full migrate from lsp-zero to builtin lsp

--- lua/elifront/plugins/lsp.lua
+++ lua/elifront/plugins/lsp.lua
@@ -1,9 +1,7 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
+    "neovim/nvim-lspconfig",
     dependencies = {
-      { "neovim/nvim-lspconfig" },
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/nvim-cmp" },
       { "zbirenbaum/copilot.lua" },
@@ -14,48 +12,51 @@ return {
       { "b0o/schemastore.nvim" },
     },
     config = function()
-      local lsp_zero = require("lsp-zero")
+      -- Setup diagnostics
+      vim.diagnostic.config({
+        virtual_text = false,
+        float = { border = "rounded" },
+        update_in_insert = false,
+        severity_sort = true,
+      })
 
-      lsp_zero.on_attach(function(_client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
-
+      -- Keymaps for LSP actions
+      local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr }
 
-        -- vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
         vim.keymap.set("n", "<leader>ld", function()
           vim.diagnostic.open_float(nil, {
             focus = false,
-            source = true, -- <--- ensures the source is shown
+            source = true,
             header = "Diagnostics",
           })
         end, opts)
+
         vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        -- <leader>yd to copy the diagnostic message of the item under the cursor
+
         vim.keymap.set("n", "<leader>yd", function()
           local diagnostic = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })[1]
-
           if diagnostic then
             vim.fn.setreg("", diagnostic.message)
-            vim.notify("Copied diagnostic message to clipboard", vim.log.levels.INFO)
+            vim.notify("Copied diagnostic message to unnamed register", vim.log.levels.INFO)
           else
-            vim.notify("No diagnostic message found under cursor", vim.log.levels.WARN)
+            vim.notify("No diagnostic message under cursor", vim.log.levels.WARN)
           end
         end, opts)
+
         vim.keymap.set("n", "<leader>cd", function()
           local diagnostic = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })[1]
-
           if diagnostic then
             vim.fn.setreg("+", diagnostic.message)
             vim.notify("Copied diagnostic message to clipboard", vim.log.levels.INFO)
           else
-            vim.notify("No diagnostic message found under cursor", vim.log.levels.WARN)
+            vim.notify("No diagnostic message under cursor", vim.log.levels.WARN)
           end
         end, opts)
-      end)
+      end
 
+      -- Setup Mason
       require("mason").setup({})
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -73,71 +74,57 @@ return {
           "tflint",
           "terraformls",
           "svelte",
-          -- "zls",
           "intelephense",
-          "gopls"
+          "gopls",
         },
         handlers = {
           function(server_name)
-            vim.lsp.config(server_name, {})
+            vim.lsp.config(server_name, {
+              on_attach = on_attach,
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            })
           end,
+
           html = function()
             vim.lsp.config("html", {
+              on_attach = on_attach,
               filetypes = { "html", "php" },
               settings = {
                 html = {
-                  format = {
-                    enable = false,
-                  },
-                  suggest = {
-                    completion = {
-                      enabled = true,
-                      emmet = true,
-                    },
-                  },
+                  format = { enable = false },
+                  suggest = { completion = { enabled = true, emmet = true } },
                 },
               },
             })
           end,
+
           jsonls = function()
             vim.lsp.config("jsonls", {
+              on_attach = on_attach,
               settings = {
                 json = {
-                  format = {
-                    enable = true,
-                  },
+                  format = { enable = true },
                   schemas = require("schemastore").json.schemas(),
                   validate = { enable = true },
                 },
               },
             })
           end,
+
           cssls = function()
             vim.lsp.config("cssls", {
+              on_attach = on_attach,
               settings = {
-                css = {
-                  validate = true,
-                  lint = {
-                    unknownAtRules = "ignore",
-                  }
-                },
-                scss = {
-                  validate = true,
-                  lint = {
-                    unknownAtRules = "ignore",
-                  }
-                },
-                less = {
-                  validate = true,
-                  lint = {
-                    unknownAtRules = "ignore",
-                  }
-                },
+                css = { validate = true, lint = { unknownAtRules = "ignore" } },
+                scss = { validate = true, lint = { unknownAtRules = "ignore" } },
+                less = { validate = true, lint = { unknownAtRules = "ignore" } },
               },
             })
           end,
+
           intelephense = function()
             vim.lsp.config("intelephense", {
+              on_attach = on_attach,
               settings = {
                 intelephense = {
                   stubs = {
@@ -155,8 +142,10 @@ return {
               },
             })
           end,
+
           ts_ls = function()
             vim.lsp.config("ts_ls", {
+              on_attach = on_attach,
               init_options = {
                 preferences = {
                   importModuleSpecifier = "non-relative",
@@ -168,33 +157,35 @@ return {
         },
       })
 
-      -- manual setup for local zls
-      do
-        if vim.fn.executable("zls") == 0 then
-          vim.notify("No locally installed zls")
-        else
-          vim.lsp.config("zls", {
-            cmd = { "zls" },
-            settings = {
-              zls = {
-                enable_build_on_save = true,
-                build_on_save_step = "check",
-                enable_snippets = true,
-                enable_ast_check_diagnostics = true,
-                enable_autofix = false,
-                enable_import_embedfile_argument_completions = true,
-                warn_style = true,
-                enable_semantic_tokens = true,
-                enable_inlay_hints = true,
-                inlay_hints_hide_redundant_param_names = true,
-                inlay_hints_hide_redundant_param_names_last_token = true,
-                operator_completions = true,
-                include_at_in_builtins = true,
-                max_detail_length = 1048576,
-              },
+      -- Optional ZLS setup
+      if vim.fn.executable("zls") == 1 then
+        vim.lsp.config("zls", {
+          on_attach = on_attach,
+          cmd = { "zls" },
+          filetypes = { "zig", "zir" },
+          settings = {
+            zls = {
+              enable_build_on_save = true,
+              build_on_save_step = "check",
+              enable_snippets = true,
+              enable_ast_check_diagnostics = true,
+              enable_autofix = false,
+              enable_import_embedfile_argument_completions = true,
+              warn_style = true,
+              enable_semantic_tokens = true,
+              enable_inlay_hints = true,
+              inlay_hints_hide_redundant_param_names = true,
+              inlay_hints_hide_redundant_param_names_last_token = true,
+              operator_completions = true,
+              include_at_in_builtins = true,
+              max_detail_length = 1048576,
             },
-          })
-        end
+          },
+        })
+
+        vim.lsp.enable("zls")
+      else
+        vim.notify("No locally installed zls", vim.log.levels.WARN)
       end
     end,
   },
